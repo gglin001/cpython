1efdb456e1c48c56fb66d9cbf313133763c39ac4
Allen Guo <guosonglin001@gmail.com>
2023-02-12 11:39:19 +0000

    add build scripts

---
+++ t.sh
@@ -0,0 +1,22 @@
+# for ubuntu2004
+apt install libffi-dev -y
+apt install make -y
+
+apt install libssl-dev
+# mamba install "openssl<2.0"
+
+pip install compiledb
+
+# export CC=clang
+
+# ./configure
+./configure --with-pydebug
+
+compiledb make -j128
+
+./python -v
+
+objdump --syms libpython3.8d.a | grep debug
+
+# Programs/python.c
+# Modules/main.c
