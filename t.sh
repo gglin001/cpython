3795e6b925a5586169f52389f20dfc6bd1f60da6
Allen Guo <guosonglin001@gmail.com>
2023-04-12 14:17:35 +0000

    stash

---
+++ t.sh
@@ -0,0 +1,25 @@
+# for ubuntu2004
+apt install libffi-dev -y
+apt install make -y
+apt install pkg-config -y
+
+apt install libssl-dev
+# mamba install "openssl<2.0"
+
+pip install compiledb
+
+export CC=clang
+
+# ./configure
+./configure --with-pydebug
+
+compiledb make -j16
+
+cp compile_commands.json build/
+
+./python -v
+
+objdump --syms libpython3.11d.a | grep debug
+
+# Programs/python.c
+# Modules/main.c
