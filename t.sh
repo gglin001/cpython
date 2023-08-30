bcaa6c833a9aad0a06cb5439e81147037436c345
Allen Guo <guosonglin001@gmail.com>
2023-08-30 08:58:44 +0000

    wip

--- t.sh
+++ t.sh
@@ -6,20 +6,22 @@ apt install pkg-config -y
 apt install libssl-dev
 # mamba install "openssl<2.0"
 
-pip install compiledb
-
 export CC=clang
 
 # ./configure
 ./configure --with-pydebug
 
-compiledb make -j16
+# pip install compiledb
+# compiledb make -j16
+
+mamba install bear abseil-cpp
+bear -- make -j16
 
 cp compile_commands.json build/
 
 ./python -v
 
-objdump --syms libpython3.11d.a | grep debug
+objdump --syms libpython3.10d.a | grep debug
 
 # Programs/python.c
 # Modules/main.c
