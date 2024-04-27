cb7ba8c13575b1e2563852350ee0acbfd5afc4e9
Allen Guo <guosonglin001@gmail.com>
2024-04-27 15:07:09 +0800

    wip

---
+++ _demos/t0.build.sh
@@ -0,0 +1,27 @@
+# for ubuntu2004
+apt install libffi-dev -y
+apt install make -y
+apt install pkg-config -y
+
+apt install libssl-dev
+# mamba install "openssl<2.0"
+
+export CC=clang
+
+# ./configure
+./configure --with-pydebug
+
+# pip install compiledb
+# compiledb make -j16
+
+mamba install bear abseil-cpp
+bear -- make -j16
+
+cp compile_commands.json build/
+
+./python -v
+
+objdump --syms libpython3.12d.a | grep debug
+
+# Programs/python.c
+# Modules/main.c
